b1ec5705a7dab435fde5f35d494d9287c1d1087f
nasir uddin <nunnasir@gmail.com>
2026-03-09 19:10:08 +0600

    EHome Page Product Showcasing

--- SimpleCommerce.Web/Controllers/HomeController.cs
+++ SimpleCommerce.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using SimpleCommerce.BLL.Interfaces;
+using SimpleCommerce.Contract;
 using SimpleCommerce.Web.Models;
 
 namespace SimpleCommerce.Web.Controllers
@@ -7,20 +9,34 @@ namespace SimpleCommerce.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var products = await _productService.GetAllAsync();
+            return View(products);
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
